aa18ebd4126f86a3b2f4c593692b03c36422c573
Anish <68448458+anish-lakkapragada@users.noreply.github.com>
2021-06-24 14:45:23 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 import setuptools
 
+import pathlib
+import sys
+from distutils.core import setup
+from os import path
+
+import setuptools
+from setuptools import setup
+
+version_name = sys.argv[1].replace("refs/tags/", "")
+del sys.argv[1]
+
 setuptools.setup(name='vidaug',
-                 version='0.1',
+                 version=version_name,
                  description='Video Augmentation Library',
                  url='https://github.com/okankop/vidaug',
                  author='Okan Kopuklu',
